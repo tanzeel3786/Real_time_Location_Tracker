a385b212e65111d6ea17a6f249444102011c05c1
tanzeel3786 <tanzilkhan3786@gmail.com>
2021-06-01 20:21:50 +0530

    Initial commit

---
+++ gradlew.bat
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding='utf-8' standalone='no'?>
+<resources xmlns:xliff="urn:oasis:names:tc:xliff:document:1.2">
+<!-- blaze-out/k8-opt/genfiles/java/com/google/android/gmscore/integ/client/base/res/values-hy/strings.xml -->
+<eat-comment/>
+<string name="common_google_play_services_enable_button" msgid="8585111718777012708">"Միացնել"</string>
+<string name="common_google_play_services_enable_text" msgid="7045129128108380659">"<xliff:g id="APP_NAME">%1$s</xliff:g> հավելվածը չի աշխատի մինչև չմիացնեք Google Play ծառայությունները:"</string>
+<string name="common_google_play_services_enable_title" msgid="3147157405986503375">"Միացնել Google Play ծառայությունները"</string>
+<string name="common_google_play_services_install_button" msgid="9115728400623041681">"Տեղադրել"</string>
+<string name="common_google_play_services_install_text" msgid="8179894293032530091">"<xliff:g id="APP_NAME">%1$s</xliff:g> հավելվածը չի աշխատի առանց Google Play ծառայությունների, որոնք չկան ձեր սարքում:"</string>
+<string name="common_google_play_services_install_title" msgid="7442193194585196676">"Տեղադրել Google Play ծառայությունները"</string>
+<string name="common_google_play_services_notification_channel_name" msgid="4293541511340436975">"Google Play ծառայությունների հասանելիություն"</string>
+<string name="common_google_play_services_notification_ticker" msgid="180394615667698242">"Google Play ծառայությունների սխալ կա"</string>
+<string name="common_google_play_services_unsupported_text" msgid="5501187292256987189">"<xliff:g id="APP_NAME">%1$s</xliff:g> հավելվածը չի աշխատի առանց Google Play ծառայությունների, որոնք ձեր սարքում չեն աջակցվում:"</string>
+<string name="common_google_play_services_update_button" msgid="8172070149091615356">"Թարմացնել"</string>
+<string name="common_google_play_services_update_text" msgid="7773943415006962566">"<xliff:g id="APP_NAME">%1$s</xliff:g> հավելվածը չի աշխատի մինչև չթարմացնեք Google Play ծառայ�
